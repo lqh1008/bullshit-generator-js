aa278cfe2fa5686c36ddee5b84deb14973bc59c0
吕启航 <lqh1023545902@163.com>
2023-12-12 17:30:58 +0800

    获取json，随机函数

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-import {generate} from './lib/generator.js';
-import {loadCorpus} from './lib/corpus.js';
-import {createRandomPicker} from './lib/random.js';
+import { readFile, readFileSync } from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+// readFile('./corpus/data.json',{encoding: 'utf-8'},(err,data)=>{
+//   if(!err){
+//     console.log(`output->data`,data)
+//   }else{
+//     console.log(`output->err`,err)
+//   }
+// })
 
-const corpus = loadCorpus('./corpus/data.json');
+// 我们使用的相对路径./corpus/data.json是相对于脚本的运行目录（即，node执行脚本的目录），而不是脚本文件的目录。
+// const data = readFileSync('./corpus/data.json',{encoding:'utf-8'})
+// console.log('🚀  data:::', data)
 
-export default (min = 200, max = 1000) => {
-  const title = createRandomPicker(corpus.title)();
-  return generate(title, {corpus, min, max});
-};
+// 当前脚本文件的 URL 地址， file:///c:/Users/lvqihang/Desktop/mime/bullshit-generator-js/index.js
+const url = import.meta.url
+console.log('🚀  url:::', url)
+// fileURLToPath：先把当前文件地址转换成路径，
+// dirname: 然后获取这个文件的所在的文件夹，
+// resolve: 最后拼接成绝对路径
+const filePath = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
+console.log('🚀  dirname(fileURLToPath(url)):::', dirname(fileURLToPath(url)))
+console.log('🚀  fileURLToPath(url):::', fileURLToPath(url))
+console.log('🚀  filePath:::', filePath)
+
+const data = readFileSync(filePath,{encoding:'utf8'})
+console.log('🚀  data:::', data)
+console.log('🚀  data:::', JSON.parse(data))
